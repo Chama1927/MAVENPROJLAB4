d1081f4da8fc897870dc1e036240fedf50bf73ca
Chama1927 <89285260+Chama1927@users.noreply.github.com>
2022-04-09 19:39:56 +0530

    Add files via upload

---
+++ LAB4/src/main/java/com/item.java
@@ -0,0 +1,45 @@
+package com;
+
+
+public class item {
+	private int itemID;
+	private String itemCode;
+	private String itemName;
+	private double itemPrice;
+	private String itemDesc;
+	public int getItemID() {
+		return itemID;
+	}
+	public void setItemID(int itemID) {
+		this.itemID = itemID;
+	}
+	public String getItemCode() {
+		return itemCode;
+	}
+	public void setItemCode(String itemCode) {
+		this.itemCode = itemCode;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public double getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public String getItemDesc() {
+		return itemDesc;
+	}
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+	
+	
+	
+	
+
+}
